fb1aa98fb4442cfc062bd8edea2e2f053b6700d8
imppotunator <khoetesl@gmail.com>
2017-04-05 11:03:13 +0200

    dependency-injection-master
    LooseCoupling

---
+++ LooseCoupling/PeopleViewer.Ninject/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PeopleViewer.Presentation;
+using PersonRepository.Caching;
+using PersonRepository.CSV;
+using PersonRepository.Service;
+using System.Windows;
+using Ninject;
+using Common;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+        IKernel Container;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            Application.Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            Container = new StandardKernel();
+            Container.Bind<IPersonRepository>().To<CSVRepository>()
+                .InSingletonScope();
+        }
+
+        private void ComposeObjects()
+        {
+            Application.Current.MainWindow = Container.Get<MainWindow>();
+        }
+    }
+}
---
+++ LooseCoupling/PeopleViewer/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PeopleViewer.Presentation;
+using PersonRepository.Caching;
+using PersonRepository.CSV;
+using PersonRepository.Service;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var wrappedRepository = new ServiceRepository();
+            var repository = new CachingRepository(wrappedRepository);
+            var viewModel = new MainWindowViewModel(repository);
+            Application.Current.MainWindow = new MainWindow(viewModel);
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
